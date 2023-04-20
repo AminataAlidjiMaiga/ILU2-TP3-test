f6d1e6f5fc64e57a809226cb732253ac33c17c42
AminataAlidjiMaiga <aminatamaiga69@yahoo.com>
2023-04-20 17:22:46 +0200

    next

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -1,9 +1,19 @@
 package ilu2;
 
 public class Welcome {
-
-	public Welcome() {
-		// TODO Auto-generated constructor stub
+	
+	public static String affichage(String nom) {
+		StringBuilder retour = new StringBuilder();
+		retour.append("Hello,");
+		if (nom.equals("bob")) {
+			retour.append("Bob");
+		}
+		return retour.toString();
 	}
-
+	
+	public static void main (String args[])
+	{
+		String nom=affichage("bob");
+		System.out.println(nom);
+	}
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -2,13 +2,21 @@ package ilu2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class WelcomeTest {
+	
+	@BeforeEach
+	public void initialisation()
+	{
+		System.out.println("Lancement des tests");
+
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test1() {
+		assertEquals(Welcome.affichage("bob"),"Hello,Bob");
 	}
 
 }
